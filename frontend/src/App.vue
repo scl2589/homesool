00a3ade5e77e321194a43d89881abc23c4844a0d
신채린 <chaelinshin96@gmail.com>
2020-10-26 00:54:11 +0900

    [S03P31A503-11] feature/develop : Delete unneeded components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h3>술이술이홈술이</h3>
     </div>
     <router-view/>
   </div>
