e24260b45eace07fbeaf873c4866b0f7f29661bf
박선환 <witty_man@naver.com>
2020-10-26 17:41:51 +0900

    [S03P31A503-112] feature/FE/meeting : Add meeting page footer design

--- frontend/src/store/modules/meetingStore.js
+++ frontend/src/store/modules/meetingStore.js
@@ -1,21 +1,40 @@
 const meetingStore = {
   namespaced: true,
   state: {
-    isMultiPanel: false,
-    isChatPanel: false,
+    isGameMode: false,
+    isMusicMode: false,
+    isAnonymousMode: false,
+    isSnapshotMode: false
   },
   getters: {
   },
   mutations: {
-    SET_ISMULTIPANEL(state, value) {
-      state.isMultiPanel = value
+    SET_ISGAME_MODE(state, value) {
+      state.isGameMode = value
     },
-    SET_ISCHATPANEL(state, value) {
-      state.isChatPanel = value
+    SET_ISMUSIC_MODE(state, value) {
+      state.isGameMode = value
+    },
+    SET_ISANONYMOUS_MODE(state, value) {
+      state.isAnonymousMode = value
+    },
+    SET_ISSNAPSHOT_MODE(state, value) {
+      state.isSnapshotMode = value
     }
   },
   actions: {
-
+    startGameMode({ commit }) {
+      commit('SET_ISGAME_MODE', true)
+    },
+    startMusicMode({ commit }) {
+      commit('SET_ISMUSIC_MODE', true)
+    },
+    startAnonymousMode({ commit }) {
+      commit('SET_ISANONYMOUS_MODE', true)
+    },
+    startSnapshotMode({ commit }) {
+      commit('SET_ISSNAPSHOT_MODE', true)
+    }
   }
 
 }
--- frontend/src/views/MeetingPage.vue
+++ frontend/src/views/MeetingPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="row no-gutters" style="height:92vh;">
+    <div class="row no-gutters" style="height:91vh;">
       <div 
         class="left-panel" 
         :class="{'col-8' : (isMultiPanel || isChatPanel), 'col-12' : !isMultiPanel && !isChatPanel }"
@@ -33,28 +33,93 @@
     </div>
 
 
-    <div class="footer" style="height:8vh;">
+    <div class="footer d-flex justify-content-between align-items-center w-100 px-2" style="height:9vh;">
+      <div>
+        <button class="btn">
+          <img src="@/assets/images/bgm.png" alt="bgm">
+        </button>
+      </div>
+
+      <div>
+        <button class="btn mr-2">
+          <img src="@/assets/images/webcam.png" alt="webcam">
+        </button>
+        <button class="btn mr-2">
+          <img src="@/assets/images/voice.png" alt="voice">
+        </button>
+        <button class="btn mr-2">
+          <img src="@/assets/images/screenshare.png" alt="screenshare">
+        </button>
+        <button class="btn mr-2" @click="startSnapshotMode">
+          <img src="@/assets/images/snapshot.png" alt="snapshot">
+        </button>
+        <div class="btn-group dropup">
+          <button type="button" class="btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="@/assets/images/funmode.png" alt="funmode">
+          </button>
+          <div class="dropdown-menu text-center">
+            <li class="dropdown-item" @click="startGameMode">술게임 모드</li>
+            <li class="dropdown-item" @click="startMusicMode">노래방 모드</li>
+            <li class="dropdown-item" @click="startAnonymousMode">진실의 방 모드</li>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button class="btn mr-2" @click="clickChatMode">
+          <img src="@/assets/images/chat.png" alt="chat">
+        </button>
+        <div class="btn-group dropup">
+          <button type="button" class="btn mr-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="@/assets/images/setting.png" alt="setting">
+          </button>
+          <div class="dropdown-menu dropdown-menu-right text-center">
+            <li class="dropdown-item">테마 변경</li>
+            <li class="dropdown-item">미팅 링크 복사</li>
+            <li class="dropdown-item">미팅 나가기</li>
+          </div>
+        </div>
+      </div>
+      
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex' 
+import { mapState, mapActions } from 'vuex' 
 export default {
   name: 'MeetingPage',
   data() {
     return {
+      isChatPanel: false
     }
   },
   computed: {
-    ...mapState('meetingStore', ['isMultiPanel', 'isChatPanel'])
+    ...mapState('meetingStore', ['isGameMode', 'isMusicMode', 'isAnonymousMode', 'isSnapshotMode']),
+    isMultiPanel() {
+      if (this.isGameMode || this.isMusicMode || this.isAnonymousMode || this.isSnapshotMode) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  methods: {
+    ...mapActions('meetingStore', ['startGameMode', 'startMusicMode', 'startAnonymousMode', 'startSnapshotMode']),
+    clickChatMode() {
+      this.isChatPanel = !this.isChatPanel
+    }
   }
 }
 </script>
 
 <style scoped>
 .left-panel {
-  background-color: red;
+  background-image: 
+    linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url('../assets/images/basic_back.png');
+  background-size: contain;
+  background-repeat: repeat;
 }
 
 .right-panel {
@@ -72,7 +137,7 @@ export default {
 }
 
 .footer {
-  background-color: pink;
+  background-color: #D1D1D1;
   height: 20vh;
 }
 
@@ -82,5 +147,14 @@ export default {
 
 .full-height {
   height: 100%;
+}
+
+.btn {
+  padding: 0;
+}
+
+.btn > img {
+  width: 7vh;
+  height: auto;
 }
 </style>
