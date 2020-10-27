ce2f424a82b84cb7b794947c3935c864ba333cab
SONMYEONGJI <ji.exitosa@gmail.com>
2020-10-27 15:17:54 +0900

    [S03P31A503-85] feature/FE/homeheader Fix to show header properly

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -40,6 +40,8 @@ export default {
             // 가입 여부 확인
             // if(존재) 방 연결
             // else 잘못된 key라고 알려주기
+            
+            this.$router.push('/meet');
         }
     },
 }
