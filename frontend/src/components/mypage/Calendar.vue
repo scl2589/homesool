44953b66a3b44951ad3c6f46af937cd929ab6c35
신채린 <chaelinshin96@gmail.com>
2020-10-28 17:53:00 +0900

    [S03P31A503-105] feature/FE/calendar : Add calendar design

--- frontend/src/components/mypage/Calendar.vue
+++ frontend/src/components/mypage/Calendar.vue
@@ -1,13 +1,127 @@
 <template>
-  
+  <div>
+    <div class="p-3 container">
+      <vc-calendar
+        class="custom-calendar"
+        :attributes="attributes"
+        disable-page-swipe
+        is-expanded
+      >
+        <template v-slot:day-content="{ day, attributes }">
+          <div class="flex flex-col h-full z-10 overflow-hidden">
+            <span class="day-label text-sm text-gray-900">{{ day.day }}</span>
+            <div class="flex-grow overflow-y-auto overflow-x-auto">
+              <p
+                v-for="attr in attributes"
+                :key="attr.customData.title"
+                class="text-xs leading-tight rounded-sm p-1 mt-0 mb-1"
+                :class="attr.customData.class"
+              >
+                {{ attr.customData.title }}
+              </p>
+            </div>
+          </div>
+        </template>
+      </vc-calendar>
+    </div>
+
+  </div>  
 </template>
 
 <script>
-export default {
 
+export default {
+  name: 'Calendar',
+  data() {
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    return {
+      attributes: [
+        {
+          key: 1,
+          customData: {
+            title: 'Lunch with mom.',
+            class: 'bg-red-600 text-white',
+          },
+          dates: new Date(year, month, 1),
+        },
+        {
+          key: 2,
+          customData: {
+            title: 'Take Noah to basketball practice',
+            class: 'bg-blue-500 text-white',
+          },
+          dates: new Date(year, month, 2),
+        },
+        {
+          key: 3,
+          customData: {
+            title: "Noah's basketball game.",
+            class: 'bg-blue-500 text-white',
+          },
+          dates: new Date(year, month, 5),
+        },
+        {
+          key: 4,
+          customData: {
+            title: 'Take car to the shop',
+            class: 'bg-indigo-500 text-white',
+          },
+          dates: new Date(year, month, 5),
+        },
+        {
+          key: 4,
+          customData: {
+            title: 'Meeting with new client.',
+            class: 'bg-teal-500 text-white',
+          },
+          dates: new Date(year, month, 7),
+        },
+        {
+          key: 5,
+          customData: {
+            title: "Mia's gymnastics practice.",
+            class: 'bg-pink-500 text-white',
+          },
+          dates: new Date(year, month, 11),
+        },
+        {
+          key: 6,
+          customData: {
+            title: 'Cookout with friends.',
+            class: 'bg-orange-500 text-white',
+          },
+          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
+        },
+        {
+          key: 7,
+          customData: {
+            title: "Mia's gymnastics recital.",
+            class: 'bg-pink-500 text-white',
+          },
+          dates: new Date(year, month, 22),
+        },
+        {
+          key: 8,
+          customData: {
+            title: 'Visit great grandma.',
+            class: 'bg-red-600 text-white',
+          },
+          dates: new Date(year, month, 25),
+        },
+      ],
+    };
+  },
 }
 </script>
 
-<style>
+<style scoped>
+p {
+  color: black !important;
+}
 
+.vc-pane-container {
+  width: 50%;
+  position: relative;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,25 @@ import store from './store'
 
 import secrets from './secrets'
 
+//  calendar
+import VCalendar from 'v-calendar';
+
+// Use v-calendar & v-date-picker components
+Vue.use(VCalendar, {
+  componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
+  locales: {
+    'ko-KR': {
+      firstDayOfWeek: 1,
+      masks: {
+        L: 'YYYY-MM-DD',
+        // ...optional `title`, `weekdays`, `navMonths`, etc
+        title: 'YYYY년 MM월',
+        dayPopover: 'YYYY년 MM월 DD일(WWW)'
+      }
+    }
+  }
+})
+
 
 // kakao login
 window.Kakao.init(secrets['KAKAO']['CLIENT_ID']);
