a9bfc9b565bcbae78fbbbe8f5dc99e86e1ac81bd
jieunchae <cj1081@naver.com>
2020-10-28 14:09:17 +0900

    [S03P31A503-52] feature/BE/picture : add File Upload

--- Backend/API/src/main/java/com/ssafy/homesool/controller/RoomController.java
+++ Backend/API/src/main/java/com/ssafy/homesool/controller/RoomController.java
@@ -1,5 +1,10 @@
 package com.ssafy.homesool.controller;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.mariadb.jdbc.internal.logging.Logger;
 import org.mariadb.jdbc.internal.logging.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,7 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -65,4 +73,61 @@ public class RoomController {
 		logger.debug(String.format("update Room {%s} end time {%s} 호출",updateRoomInfo.getCode(), updateRoomInfo.getEndTime().toString()));
 		return new ResponseEntity<Room>(roomService.update(updateRoomInfo.getCode(), updateRoomInfo.getEndTime()),HttpStatus.OK);
 	}
+	
+	@PostMapping("upload")
+	@ApiOperation(value = "사진 업로드", notes = "서버에 스크린샷을 저장한다.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "OK"),
+		@ApiResponse(code = 400, message = "Bad Request"),
+		@ApiResponse(code = 401, message = "Unauthorized"),
+		@ApiResponse(code = 403, message = "Forbidden"),
+		@ApiResponse(code = 404, message = "Not Found")
+	})
+	private ResponseEntity<String> doFileUpload(
+			@ApiParam(value = "업로드할 사진 정보") @RequestPart("img") MultipartFile uploadfile) {
+			logger.debug("사진 업로드 시작\n");
+			String filename = "";
+			String access_path="";
+			try {
+				// 업로드되는 파일 이름 중간에 날짜줄거임
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+				Date nowdate = new Date();
+				String dateString = formatter.format(nowdate);
+
+				// 웹서비스 경로 지정
+				// String root_path =
+				// request.getSession().getServletContext().getRealPath("\\");
+
+				String root_path = "C:\\Users\\multicampus\\git\\test\\";
+				String real_path = "/home/ubuntu/picture/";
+				// String attach_path = "resources\\upload\\";
+				filename = dateString + "_" + uploadfile.getOriginalFilename();
+
+				System.out.println(root_path + filename);
+
+				FileOutputStream fos = new FileOutputStream(root_path + filename);
+				// 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
+				InputStream is = uploadfile.getInputStream();
+				// file로 부터 inputStream을 가져온다.
+
+				int readCount = 0;
+				byte[] buffer = new byte[2048];
+				// 파일을 읽을 크기 만큼의 buffer를 생성하고
+				// ( 보통 1024, 2048, 4096, 8192 와 같이 배수 형식으로 버퍼의 크기를 잡는 것이 일반적이다.)
+
+				while ((readCount = is.read(buffer)) != -1) {
+					// 파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
+					fos.write(buffer, 0, readCount);
+					// 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
+				}
+
+				access_path = "http:/k3a503.p.ssafy.io/images/";
+
+			} catch (Exception ex) {
+				throw new RuntimeException("file Save Error");
+			}
+
+			return new ResponseEntity<>(access_path + filename, HttpStatus.OK);
+	}
+	
 }
