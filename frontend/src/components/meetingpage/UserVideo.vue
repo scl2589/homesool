f0709292ef3098123729bcf473c27cf12e4e77e3
jieunchae <cj1081@naver.com>
2020-11-13 04:01:00 +0900

    [S03P31A503-190] feature/FE/drinkCounting : Add drink-overlay and axios
    현재 주종에 관한 주량 기록 통신
    +를 누르면 하나 증가, -를 누르면 하나 감소

--- frontend/src/components/meetingpage/UserVideo.vue
+++ frontend/src/components/meetingpage/UserVideo.vue
@@ -1,19 +1,26 @@
 <template>
 <div v-if="streamManager">
-	<div>
+	<div class="container">
+		<div v-if="!($route.name==='HomePage') && streamManager && publisher">
+			<div class="drink-overlay" v-if="streamManager.stream.connection.connectionId==publisher.stream.connection.connectionId">
+				<img class="drink-minus" src="@/assets/images/minus.png" alt="한잔 덜 마셨어요" @click="updateUserDrinkRecord(-1)"> 
+				<img class="drink" :src="require(`@/assets/images/${currentDrink}.png`)" alt="${currentDrink}">
+				<img class="drink-plus" src="@/assets/images/plus.png" alt="한잔 더 마셨어요" @click="updateUserDrinkRecord(1)">
+			</div>
+		</div>
 		<div class="overlay d-flex justify-content-center align-items-center" v-if="currentMode === 'anonymous'">
 			<img height="100%" src="@/assets/images/host.png" alt="">
 		</div>
 		<ov-video :stream-manager="streamManager"/>
 	</div>
-	<div v-if="!$route.name==='HomePage'">
-		<p>{{clientData}}</p>
+	<div v-if="!($route.name==='HomePage')">
+		<p>{{user.drinks[0]}}</p>
 	</div>
 </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState , mapActions } from 'vuex';
 import OvVideo from './OvVideo';
 export default {
 	name: 'UserVideo',
@@ -24,17 +31,24 @@ export default {
 		streamManager: Object,
 	},
 	computed: {
-		...mapState('meetingStore', ['currentMode']),
+		...mapState('meetingStore', ['currentMode','user','currentDrink','publisher']),
+		...mapState(['user']),
 		clientData () {
 			const { clientData } = this.getConnectionData();
 			return clientData;
 		},
 	},
 	methods: {
+		...mapActions('meetingStore', ['updateUserDrinkRecord']),
 		getConnectionData () {
 			const { connection } = this.streamManager.stream;
 			return JSON.parse(connection.data);
 		},
+	},
+	data(){
+		return{
+			imgsrc: "@/assets/images/soju.png",
+		}
 	},
 };
 </script>
@@ -53,4 +67,69 @@ p {
   background-color: rgba(0,0,0,1); /*dim the background*/
 }
 
+.drink-overlay{
+ position: absolute;
+ top: 0;
+ bottom: 0;
+ left: 0;
+ right: 0;
+ height: 100%;
+ opacity: 1;
+ transition: .3s ease;
+}
+
+.drink{
+  background-color: white;
+  border-radius: 50%;
+  width: 7%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+  z-index: 20;
+  opacity: 0;
+  padding : 10px;
+}
+
+.drink-minus{
+  background-color: white;
+  border-radius: 50%;
+  width: 5%;
+  position: absolute;
+  top: 50%;
+  left: 40%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+  z-index: 20;
+  opacity: 0;
+  padding : 10px;
+}
+
+.drink-plus{
+  background-color: white;
+  border-radius: 50%;
+  width: 5%;
+  position: absolute;
+  top: 50%;
+  left: 60%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+  z-index: 20;
+  opacity: 0;
+  padding : 10px
+}
+
+.container:hover .drink{
+	opacity: 0.8;
+}
+.container:hover .drink-minus{
+	opacity: 0.4;
+}
+.container:hover .drink-plus{
+	opacity: 0.4;
+}
 </style>
