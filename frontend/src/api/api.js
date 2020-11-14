fb530814b5c417e894f4b95739f59632ec02ddf2
jieunchae <cj1081@naver.com>
2020-11-15 05:18:09 +0900

    [S03P31A503-121] feature/FE/drinkCount : fix url
    ㅎㅎ,,,

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,8 @@ export default {
   URL: 'https://k3a503.p.ssafy.io:8889',
   //URL: 'http://localhost:8888',
   YOUTUBE_URL: "https://www.googleapis.com/youtube/v3/search",
-  //OPENVIDU_URL: "https://k3a503.p.ssafy.io",
-  OPENVIDU_URL: "https://192.168.219.108:4443",
+  OPENVIDU_URL: "https://k3a503.p.ssafy.io",
+  //OPENVIDU_URL: "https://192.168.219.108:4443",
   ROUTES: {
     user: '/user',
     login: '/user/login',
