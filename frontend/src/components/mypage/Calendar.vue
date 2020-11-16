d289e4b4818820ef65b335959233a40bc5078c76
Lin <chaelinshin96@gmail.com>
2020-11-16 12:28:43 +0900

    [S03P31A503-203] feature/FE/calendarmodal : Update calendar modal design

--- frontend/src/components/mypage/Calendar.vue
+++ frontend/src/components/mypage/Calendar.vue
@@ -27,42 +27,52 @@
       </vc-calendar>
     </div>
     <v-dialog
-        v-model="meetingLogDialog"
-        persistent
-        max-width="600px"
+      v-model="meetingLogDialog"
+      persistent
+      max-width="600px"
+      max-height="80vh"
+        
       >
-      <v-card>
+      <v-card class="d-flex flex-column justify-content-between scroll-sect">
         <v-card-title class="d-flex justify-content-center">
-          <h3>미팅 기록 조회</h3>
+          <h4>
+            <span v-if="logs.roomInfo">
+              {{`${logs.roomInfo.startTime.slice(0, 4)}년 ${logs.roomInfo.startTime.slice(5, 7)}월 ${logs.roomInfo.startTime.slice(8, 10)}일`}}
+            </span>
+            <span class="detail-title">술자리 기록</span> 
+            
+          </h4>
+          <hr>
         </v-card-title>
         
+        
         <v-container class="d-flex flex-column align-items-start px-5 py-0">
-          <v-row class="d-flex mb-3 mx-2" v-if="logs.roomInfo">
-            <h4 class="mb-0">날짜:</h4>
-            <p class="mb-0 ml-3">{{`${logs.roomInfo.startTime.slice(0, 4)}년 ${logs.roomInfo.startTime.slice(5, 7)}월 ${logs.roomInfo.startTime.slice(8, 10)}일`}}</p>
+          <v-row class="d-flex mb-3 no-gutters" v-if="logs.roomInfo">
+            <h4 class="mb-0">{{logs.roomInfo.roomName}}</h4>
           </v-row>
-          <v-row class="d-flex mb-3 mx-2" v-if="logs.roomInfo">
-            <h4 class="mb-0">방제목:</h4>
-            <p class="mb-0 ml-3">{{logs.roomInfo.roomName}}</p>
-          </v-row>
-          <v-row class="d-flex mb-3 mx-2">
-            <h4 class="mb-0">호스트:</h4>
+          <v-row class="d-flex mb-3 mx-2 no-gutters" v-if="logs.host">
+            <h5 class="mb-0">호스트:</h5>
             <p class="mb-0 ml-3">{{logs.host}}</p>
           </v-row>
-          <v-row class="d-flex mb-3 mx-2" v-if="logs.users && logs.users.length">
-            <h4 class="mb-0">참여자:</h4>
-            <p class="mb-0 ml-3" v-for="user in logs.users" :key="user">
-              {{user}},
+          <v-row class="d-flex mb-3 mx-2 no-gutters" v-if="logs.users && logs.users.length">
+            <h5 class="mb-0">참여자 <img width="30px" src="@/assets/images/friends.png" alt="participants"></h5>
+            <p class="mb-0 ml-2" v-for="user in logs.users" :key="user">
+              <span class="badge badge-participants">{{user}}</span>
             </p>
           </v-row>
-          <v-row class="d-flex mb-3 mx-2">
-            <h4 class="mb-0">음주량:</h4>
+          <v-row class="d-flex mb-3 mx-2 no-gutters">
+            <h5 class="mb-0 mr-2">음주량 <img width="30px" src="@/assets/images/beer.png" alt="shot"> </h5>
             <span v-for="(record,index) in logs.records" :key="index">
-              <p class="mb-0 ml-3" v-if="record.liquorLimit > 0">{{record.liquorName}} {{record.liquorLimit}}잔,</p>
+              <p 
+                class="mb-0 mr-2" 
+                v-if="record.liquorLimit > 0"
+              >
+                <span class="badge badge-drinks">{{record.liquorName}} {{record.liquorLimit}}잔</span>
+              </p>
             </span>            
           </v-row>
           <v-row class="mb-3 mx-2" v-if="logs.roomInfo && logs.srcs.length">
-            <h4 class="mb-0">사진</h4>
+            <h5 class="mb-0">사진</h5>
             <v-carousel
               v-model="model"
               dark
@@ -84,7 +94,7 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
-            color="blue darken-1"
+            color="indigo"
             text
             @click="clickClose"
           >
@@ -235,5 +245,62 @@ export default {
 
 .vc-grid-cell {
   border: 1px solid rgb(0, 0, 0, 0.2) !important;
+}
+
+.detail-title {
+  color: #b0a2c8;
+}
+
+h4 {
+  font-family: 'Do Hyeon', sans-serif;
+}
+
+
+.scroll-sect {
+  max-height: 70vh !important;
+  overflow: auto;
+}
+
+.scroll-sect::-webkit-scrollbar {
+  width: 8px; 
+  height: 8px;
+}
+
+.scroll-sect::-webkit-scrollbar-track {
+  background: #37474F;
+}
+
+.scroll-sect::-webkit-scrollbar-corner {
+  background: #37474F; 
+}
+
+.scroll-sect::-webkit-scrollbar-thumb {
+  background: #b0a2c8;
+}
+
+.scroll-sect::-webkit-scrollbar-button {
+  background-color: #37474F;
+  height: 0;
+}
+
+.badge-participants {
+  background-color: #f48fb1;
+  color: white;
+  font-size: 0.9rem;
+}
+
+.badge-drinks {
+  background-color: #ffcc80;
+  color: white;
+  font-size: 0.9rem;
+}
+
+hr {
+  margin-bottom: 5px;
+  margin-top: 0;
+}
+
+button:focus {
+  outline: none;
 }
 </style>
