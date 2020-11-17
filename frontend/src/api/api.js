0a27ff34f98123fbd5b957e92e237cec7de3965b
채지은 <cj1081@naver.com>
2020-11-17 12:40:36 +0000

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 export default {
-  //URL: 'https://k3a503.p.ssafy.io:8889',
-  URL: 'http://localhost:8888',
+  URL: 'https://k3a503.p.ssafy.io:8889',
+  //URL: 'http://localhost:8888',
   YOUTUBE_URL: "https://www.googleapis.com/youtube/v3/search",
   OPENVIDU_URL: "https://k3a503.p.ssafy.io",
   //OPENVIDU_URL: "https://192.168.219.108:8443",
