e1347a3a20d5764290ff1c5eaec8a3bf4f92c011
Chae Lin Shin <chaelinshin96@gmail.com>
2021-02-02 16:33:56 +0900

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 export default {
-  URL: 'https://k3a503.p.ssafy.io:8889',
+  URL: 'https://t4coach35.p.ssafy.io:8889',
   YOUTUBE_URL: "https://www.googleapis.com/youtube/v3/search",
-  OPENVIDU_URL: "https://k3a503.p.ssafy.io",
+  OPENVIDU_URL: "https://t4coach35.p.ssafy.io",
   ROUTES: {
     user: '/user',
     login: '/user/login',
